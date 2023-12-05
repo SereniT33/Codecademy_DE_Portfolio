080064919061ac87dfd9f232eb645b24e97b74e7
SereniT33 <serenachoi1992@gmail.com>
2023-12-04 16:51:31 -0800

    Modify view tables

--- bike-rental-data-management/views.sql
+++ bike-rental-data-management/views.sql
@@ -45,16 +45,40 @@ ORDER BY daily_trips.date_key;
 
 --- create a view with montlhly trips + weather information (monthly_data)
 CREATE VIEW monthly_data AS
-SELECT daily_data.month
+SELECT dates.month
     , SUM(daily_data.total_trips) AS total_trips
     , ROUND(AVG(daily_data.total_trips)) AS avg_daily_trips
     , SUM(daily_data.subscriber_trips) AS total_subscriber_trips
     , ROUND(AVG(daily_data.subscriber_trips)) AS avg_subscriber_trips
-    , SUM(daily_data.customer_trips_trips) AS total_customer_trips
+    , SUM(daily_data.customer_trips) AS total_customer_trips
     , ROUND(AVG(daily_data.customer_trips)) AS avg_customer_trips
-    , SUM(daily_data.unknown_trips_trips) AS total_unknown_trips
+    , SUM(daily_data.unknown_trips) AS total_unknown_trips
     , ROUND(AVG(daily_data.unknown_trips)) AS avg_unkown_trips
     , SUM(daily_data.late_return) AS toal_late_return
     , ROUND(AVG(daily_data.tavg)) AS avg_tavg
-    , COUNT(daily_data.snow) FILTER (WHERE daily_data.snow=)
+    , COUNT(daily_data.snow) FILTER (WHERE daily_data.snow=True) AS snow_days
+    , COUNT(daily_data.rain) FILTER (WHERE daily_data.rain=True) AS rain_days
+    , MAX(daily_data.snow_amount) AS max_snow_amount
+    , MAX(daily_data.prcp) AS max_prcp
+FROM dates
+JOIN daily_data ON dates.date_key = daily_data.date_key
+GROUP BY dates.month
+ORDER BY dates.month;
+
 --- create a view to highlight the trips that went beyond the valid duration
+CREATE VIEW late_return AS
+SELECT dates.date
+    , trips.id
+    , trips.trip_duration
+    , trips.bike_id
+    , [( SELECT stations.name
+          FROM stations
+         WHERE trips.start_station_id = stations.id)] AS start_location
+    , [( SELECT stations.name
+          FROM stations
+         WHERE trips.end_station_id = stations.id)] AS end_location
+    , users.user_type
+FROM trips
+JOIN dates ON trips.date_key = dates.date_key
+JOIN users ON trips.id = users.trip_id
+WHERE trips.exceed_trip_duration = True; 
